e84cf07c907e6c1e7a648fe92fa5ba390c7a3806
kathy zhang <kzhang193@gmail.com>
2018-11-23 17:16:06 -0600

    initialize firebase

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDQq71i66gacNX7bUhyuRJa46Bow3B5K_U",
+    authDomain: "hw-7-train-schedule-a2cff.firebaseapp.com",
+    databaseURL: "https://hw-7-train-schedule-a2cff.firebaseio.com",
+    projectId: "hw-7-train-schedule-a2cff",
+    storageBucket: "hw-7-train-schedule-a2cff.appspot.com",
+    messagingSenderId: "271840703273"
+  };
+  firebase.initializeApp(config);
