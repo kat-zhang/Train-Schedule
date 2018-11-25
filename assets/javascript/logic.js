afaa62745d95a3129ecda20f6eaaca163637e364
kathy zhang <kzhang193@gmail.com>
2018-11-25 04:42:31 -0600

    i tried

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,33 +6,36 @@ var config = {
     projectId: "hw-7-train-schedule-a2cff",
     storageBucket: "hw-7-train-schedule-a2cff.appspot.com",
     messagingSenderId: "271840703273"
-  };
-  firebase.initializeApp(config);
+};
+
+firebase.initializeApp(config);
 
 var database = firebase.database();
+
 
 $("#add-train").on("click", function (event){
     event.preventDefault();
 
     var trainName = $("#train-name").val().trim();
     var destination =$("#destination").val().trim();
-    var firstTrain = $("#first-train").val().trim();
+    var trainArrival = $("#train-arrival").val().trim();
     var waitTime = $("#wait-time").val().trim();
 
     var newTrain = {
         name: trainName,
         destination: destination,
-        first: firstTrain,
+        first: trainArrival,
         frequency: waitTime,
     };
-// push to firebase 
+
+ // push to firebase 
     database.ref().push(newTrain);
 
     alert("New train has been added");
-
+ //empties input boxes after info submitted
     $("#train-name").val("");
     $("#destination").val("");
-    $("#first-train").val("");
+    $("#train-arrival").val("");
     $("#wait-time").val("");
 });
 
@@ -41,35 +44,24 @@ database.ref().on("child_added", function(childSnapshot) {
 
     var trainName = childSnapshot.val().name;
     var destination =childSnapshot.val().destination;
-    var firstTrain = childSnapshot.val().first;
+    var trainArrival = childSnapshot.val().first;
     var waitTime = childSnapshot.val().frequency;
-//firstTrain arrival time PLUS frequency = nextArrival
-    //push back 1 year to make it come before current time
-    // var fixFirstTrainTime = moment.unix(firstTrain).format("HH:mm");
-    // var nextArrival = moment()
-    // console.log(trainName);
-    // console.log(destination);
-    // console.log(firstTrain);
-    // console.log(frequency);
-
-    
-    // var convertTime = moment(firstTrain, "HH:MM").subtract(1, "years");
-
-    // var currentTime = moment();
-    // $("#current-time").text("CURRENT TIME: " + moment(currentTime).format("HH:mm:ss dddd, MMM D YYYY"));
-    // var nextArrival = 
-    
 
 
-    var newRow = $("<tr>").append(
+    var convertTrainArrival = moment(trainArrival, "HH:mm").subtract(1, "years");
+    var diffTime = moment().diff(moment(convertTrainArrival), "minutes");
+    var tRemainder = diffTime % waitTime;
+    var tMinutesTillTrain = waitTime - tRemainder;
+    var nextTrain = moment(convertTrainArrival).add(tMinutesTillTrain, "minutes");
+
+
+    var newRow = $("<tr>").prepend(
         $("<td>").text(trainName),
         $("<td>").text(destination),
         $("<td>").text(waitTime),
-        $("<td>").text(firstTrain),
-        // $("<td>").text(),
-
-       
+        $("<td>").text(moment(nextTrain).format("hh:mm")),
     );
-    $("#schedule-table > tbody").append(newRow)
+
+    $("#train-table > tbody").append(newRow);
 });    
      
